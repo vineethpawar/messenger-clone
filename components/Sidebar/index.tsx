473ab3fbfc8d0db886bdd41750c33d4ce22d7a15
vineethpawar <vineethpawar99@gmail.com>
2023-06-01 06:58:41 +0530

    feat: add status slider layout

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,11 +1,13 @@
 import { VStack } from "native-base";
 import React from "react";
 import Searchbar from "./Searchbar";
+import StatusSlider from "./StatusSlider";
 
 const Sidebar = () => {
   return (
     <VStack w={"100%"} maxW={"300px"}>
       <Searchbar />
+      <StatusSlider />
     </VStack>
   );
 };
