4bef6b162abbeeacbf183303b3a31cf79d57213a
vineethpawar <vineethpawar99@gmail.com>
2023-06-02 23:36:01 +0530

    chore: make file changes to home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,8 @@
-import { Box, HStack } from "native-base";
 import React from "react";
-import Sidebar from "../components/Sidebar";
+import Home from "../screens/Home";
 
 const App: React.FC = () => {
-  return (
-    <HStack p={{ base: 3, lg: 7 }} h={"100vh"} w={"100%"} bg="appBG">
-      <Box p={4} minW={"340px"} maxW={"400px"} flex={1}>
-        <Sidebar />
-      </Box>
-      <Box flex={3}></Box>
-      <Box flex={1}></Box>
-    </HStack>
-  );
+  return <Home />;
 };
 
 export default App;
---
+++ screens/Home/index.tsx
@@ -0,0 +1,20 @@
+import { HStack, Box } from "native-base";
+import React from "react";
+import Sidebar from "../../components/Sidebar";
+import MessageDetailsGroup from "../../components/MessageDetailsGroup";
+
+const Home = () => {
+  return (
+    <HStack p={{ base: 3, lg: 7 }} h={"100vh"} w={"100%"} bg="appBG">
+      <Box p={4} minW={"340px"} maxW={"400px"} flex={1}>
+        <Sidebar />
+      </Box>
+      <Box flex={3}></Box>
+      <Box p={4} flex={1} minW={"340px"} maxW={"400px"}>
+        <MessageDetailsGroup />
+      </Box>
+    </HStack>
+  );
+};
+
+export default Home;
