88ce550c66f3bb8601b08f428dbae9241e342816
vineethpawar <vineethpawar99@gmail.com>
2023-06-02 12:57:56 +0530

    chore: fix skeleton loading spacing and alignment - Sidebar

--- components/Sidebar/MessageList.tsx
+++ components/Sidebar/MessageList.tsx
@@ -1,26 +1,67 @@
 import React from "react";
-import { Box, HStack, ScrollView, Skeleton, VStack } from "native-base";
+import {
+  Box,
+  HStack,
+  Pressable,
+  ScrollView,
+  Skeleton,
+  Text,
+  VStack,
+} from "native-base";
 import MessageListItem from "../common/MessageListItem";
+import IonIcon from "@reacticons/ionicons";
 
 const MessageList = () => {
   return (
-    <ScrollView flex={1}>
-      <MessageListItem />
-    </ScrollView>
+    <>
+      <HStack alignItems={"center"} p={3}>
+        <Text
+          fontSize={18}
+          fontWeight={500}
+          fontFamily={"Lato"}
+          color={"white"}
+          flex={3}
+        >
+          Messages <IonIcon name="chevron-down-outline" />
+        </Text>
+        <Pressable
+          _hover={{ bg: "gray.600" }}
+          rounded={"full"}
+          p={2}
+          mr={-2}
+          onPress={() => {}}
+        >
+          <IonIcon
+            style={{ fontSize: "20px", color: "white" }}
+            name="person-add-outline"
+          />
+        </Pressable>
+      </HStack>
+      <ScrollView showsVerticalScrollIndicator={false} flex={1}>
+        <MessageListItem />
+        <MessageListItem isSelected />
+        <MessageListItem />
+        <MessageListItem />
+        <MessageListItem />
+        <MessageListItem />
+        <MessageListItem />
+        <MessageListItem />
+      </ScrollView>
+    </>
   );
 };
 
 export const MessageListSkeletonUI = () => {
   const SkeletonListItem = () => {
     return (
-      <HStack mb={2} space={2} alignItems={"center"}>
+      <HStack mb={9} space={2} alignItems={"center"}>
         <Skeleton
           rounded={"full"}
           size={"70px"}
           startColor={"gray.500"}
           endColor={"gray.700"}
         />
-        <VStack space={1} alignItems={"center"} flex={1}>
+        <VStack space={4} alignItems={"center"} flex={1}>
           <Skeleton
             h="20px"
             rounded="full"
@@ -38,18 +79,24 @@ export const MessageListSkeletonUI = () => {
     );
   };
   return (
-    <ScrollView showsVerticalScrollIndicator={false} flex={1}>
-      <SkeletonListItem />
-      <SkeletonListItem />
-      <SkeletonListItem />
-      <SkeletonListItem />
-      <SkeletonListItem />
-      <SkeletonListItem />
-      <SkeletonListItem />
-      <SkeletonListItem />
-      <SkeletonListItem />
-      <SkeletonListItem />
-    </ScrollView>
+    <>
+      <Skeleton
+        my={6}
+        rounded={"full"}
+        h="25px"
+        w={"100%"}
+        startColor={"gray.500"}
+        endColor={"gray.700"}
+      />
+      <ScrollView px={2} showsVerticalScrollIndicator={false} flex={1}>
+        <SkeletonListItem />
+        <SkeletonListItem />
+        <SkeletonListItem />
+        <SkeletonListItem />
+        <SkeletonListItem />
+        <SkeletonListItem />
+      </ScrollView>
+    </>
   );
 };
 
