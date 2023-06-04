894b2c6d8b7ba50b1a50dbc0b14ca9f8e8f2df2c
vineethpawar <vineethpawar99@gmail.com>
2023-06-05 01:59:36 +0530

    feat: add chat item

--- components/MessageChatSection/MessageChatBody.tsx
+++ components/MessageChatSection/MessageChatBody.tsx
@@ -1,8 +1,50 @@
-import { VStack } from "native-base";
-import React from "react";
+import { ScrollView } from "native-base";
+import React, { useEffect, useRef, useState } from "react";
+import ChatItem from "../common/ChatItem";
 
 const MessageChatBody = () => {
-  return <VStack flex={1}></VStack>;
+  const scrollViewRef = useRef();
+  const [contentHeight, setContentHeight] = useState(0);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleContentSizeChange = (contentWidth: number, height: number) => {
+    setContentHeight(height);
+  };
+
+  useEffect(() => {
+    if (
+      scrollViewRef.current &&
+      (scrollPosition <= contentHeight || scrollPosition === 0)
+    ) {
+      // @ts-ignore
+      scrollViewRef?.current?.scrollToEnd({ animated: true });
+    }
+  }, [contentHeight, scrollPosition]);
+
+  return (
+    <ScrollView
+      px={2}
+      ref={scrollViewRef}
+      showsVerticalScrollIndicator={false}
+      _contentContainerStyle={{ mt: "auto", mb: "0" }}
+      flex={1}
+      onContentSizeChange={handleContentSizeChange}
+      //   onScroll={handleScroll}
+    >
+      <ChatItem isMe />
+      <ChatItem />
+      <ChatItem />
+      <ChatItem />
+      <ChatItem />
+      <ChatItem isMe />
+      <ChatItem />
+      <ChatItem />
+      <ChatItem isMe />
+      <ChatItem />
+      <ChatItem />
+      <ChatItem isMe />
+    </ScrollView>
+  );
 };
 
 export default MessageChatBody;
