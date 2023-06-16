6062a3c9e69a16675a66c5deef6a61189b2a223d
vineethpawar <vineethpawar99@gmail.com>
2023-06-16 22:29:44 +0530

    refactor: add key to map functions

--- components/MessageChatSection/MessageChatHeader.tsx
+++ components/MessageChatSection/MessageChatHeader.tsx
@@ -56,6 +56,7 @@ const MessageChatHeader = () => {
             max={3}
           >
             <Avatar
+              key={1}
               bg="green.500"
               source={{
                 uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
@@ -64,6 +65,7 @@ const MessageChatHeader = () => {
               AJ
             </Avatar>
             <Avatar
+              key={2}
               bg="cyan.500"
               source={{
                 uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
@@ -72,6 +74,7 @@ const MessageChatHeader = () => {
               TE
             </Avatar>
             <Avatar
+              key={3}
               bg="indigo.500"
               source={{
                 uri: "https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
@@ -80,6 +83,7 @@ const MessageChatHeader = () => {
               JB
             </Avatar>
             <Avatar
+              key={4}
               bg="amber.500"
               source={{
                 uri: "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
@@ -88,6 +92,7 @@ const MessageChatHeader = () => {
               TS
             </Avatar>
             <Avatar
+              key={5}
               bg="green.500"
               source={{
                 uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
@@ -96,28 +101,13 @@ const MessageChatHeader = () => {
               AJ
             </Avatar>
             <Avatar
+              key={6}
               bg="cyan.500"
               source={{
                 uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
               }}
             >
               TE
-            </Avatar>
-            <Avatar
-              bg="indigo.500"
-              source={{
-                uri: "https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-              }}
-            >
-              JB
-            </Avatar>
-            <Avatar
-              bg="amber.500"
-              source={{
-                uri: "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-              }}
-            >
-              TS
             </Avatar>
           </Avatar.Group>
         </Box>
